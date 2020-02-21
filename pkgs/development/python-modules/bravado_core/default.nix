42d2af750c09935c99bc7b2543ea9acff8dafec9
Klaas van Schelven <klaas@vanschelven.com>
2020-02-21 10:22:45 +0100

    pythonPackages.bravado-core: 5.12.1 -> 5.16.1

--- pkgs/development/python-modules/bravado_core/default.nix
+++ pkgs/development/python-modules/bravado_core/default.nix
@@ -1,17 +1,19 @@
 { lib, buildPythonPackage, fetchPypi, python-dateutil, jsonref, jsonschema,
   pyyaml, simplejson, six, pytz, msgpack, swagger-spec-validator, rfc3987,
-  strict-rfc3339, webcolors  }:
+  strict-rfc3339, webcolors, mypy_extensions }:
 
 buildPythonPackage rec {
   pname = "bravado-core";
-  version = "5.12.1";
+  version = "5.16.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0qhisiays582xagndp3vgby6j7ppy6rivw9ycbcc776p0w8s9l21";
+    sha256 = "1vcc5wz8xbhhgd1yvdi8cdvrnkzkb8ycshsgyfisqsrvklbiss72";
   };
 
-  patches = ./setup.py.patch;  # until https://github.com/Yelp/bravado-core/pull/368 is merged
+  checkInputs = [
+    mypy_extensions
+  ];
 
   propagatedBuildInputs = [
     python-dateutil
